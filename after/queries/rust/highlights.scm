c5baa9adaf1ff70b6e6c8691d83c47ae2c0460ad
Raphael <glephunter@gmail.com>
2023-05-14 20:45:22 +0800

    update

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -2,3 +2,7 @@
 
 (enum_item
   name: (type_identifier) @enum)
+
+(use_declaration
+  (scoped_identifier
+    name: (identifier) @namespace))
--- tests/mutchar_spec.lua
+++ tests/mutchar_spec.lua
@@ -1,6 +1,7 @@
 local mutchar = require('mutchar')
 local ctx = require('mutchar.context')
 local ns = vim.api.nvim_create_namespace('mutchar')
+local eq = assert.equal
 
 mutchar.setup({
   cpp = {
@@ -24,6 +25,29 @@ local t = function(s)
   return vim.api.nvim_replace_termcodes(s, true, true, true)
 end
 
+local function join_paths(...)
+  local path_sep = on_windows and '\\' or '/'
+  local result = table.concat({ ... }, path_sep)
+  return result
+end
+
+local function treesitter_dep()
+  local data_path = vim.fn.stdpath('data')
+
+  local package_root = join_paths(data_path, 'test')
+  local treesitter_path = join_paths(package_root,'nvim-treesitter')
+  vim.opt.runtimepath:append(treesitter_path)
+
+  if vim.fn.isdirectory(treesitter_path) ~= 1 then
+    vim.fn.system({ 'git', 'clone', 'https://github.com/nvim-treesitter/nvim-treesitter',treesitter_path })
+  end
+  require('nvim-treesitter').setup({
+    ensure_installed = {'rust'},
+    sync_install = true,
+    highlight = { enable = true }
+  })
+end
+
 describe('mutchar', function()
   local bufnr
   before_each(function()
@@ -37,7 +61,7 @@ describe('mutchar', function()
     vim.api.nvim_win_set_cursor(0, { 1, 8 })
     vim.api.nvim_feedkeys(t('a,'), 'x', false)
     local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[1]
-    assert.equal('template <>', line)
+    eq('template <>', line)
   end)
 
   it('semicolon in lua', function()
@@ -46,7 +70,7 @@ describe('mutchar', function()
     vim.api.nvim_win_set_cursor(0, { 1, 13 })
     vim.api.nvim_feedkeys(t('a;'), 'x', false)
     local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[1]
-    assert.equal('function self:', line)
+    eq('function self:', line)
   end)
 
   it('single variable define in go', function()
@@ -65,7 +89,7 @@ describe('mutchar', function()
     })
     vim.api.nvim_feedkeys(t('a;'), 'x', false)
     local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[1]
-    assert.equal('g := ', line)
+    eq('g := ', line)
   end)
 
   it('multiple variable define in go', function()
@@ -86,35 +110,38 @@ describe('mutchar', function()
     })
     vim.api.nvim_feedkeys(t('a;'), 'x', false)
     local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[1]
-    assert.equal('g,t := ', line)
+    eq('g,t := ', line)
   end)
 
-  it('rust double colon', function()
-    vim.bo.filetype = 'rust'
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'fn main () {', '    let s = String', '}' })
-    vim.api.nvim_win_set_cursor(0, { 2, 18 })
-    vim.api.nvim_feedkeys(t('a;'), 'x', false)
-    local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[2]
-    assert.equal('    let s = String::', line)
-  end)
+  -- there need test with treesitter
+  describe('in rust with rust_double_colon', function()
+    it('after String keyword', function()
+      vim.bo.filetype = 'rust'
+      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'fn main () {', '    let s = String', '}' })
+      vim.api.nvim_win_set_cursor(0, { 2, 18 })
+      vim.api.nvim_feedkeys(t('a;'), 'x', false)
+      local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[2]
+      eq('    let s = String::', line)
+    end)
 
-  it('rust colon in struct', function()
-    vim.bo.filetype = 'rust'
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'struct Test {', '    username', '}' })
-    vim.api.nvim_win_set_cursor(0, { 2, 11 })
-    vim.diagnostic.set(ns, bufnr, {
-      {
-        bufnr = bufnr,
-        lnum = 1,
-        end_lnum = 2,
-        col = 12,
-        end_col = 12,
-        severity = 1,
-        message = 'expected COLON',
-      },
-    })
-    vim.api.nvim_feedkeys(t('a;'), 'x', false)
-    local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[2]
-    assert.equal('    username:', line)
+    it('in struct with diagnsotic', function()
+      vim.bo.filetype = 'rust'
+      vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'struct Test {', '    username', '}' })
+      vim.api.nvim_win_set_cursor(0, { 2, 11 })
+      vim.diagnostic.set(ns, bufnr, {
+        {
+          bufnr = bufnr,
+          lnum = 1,
+          end_lnum = 2,
+          col = 12,
+          end_col = 12,
+          severity = 1,
+          message = 'expected COLON',
+        },
+      })
+      vim.api.nvim_feedkeys(t('a;'), 'x', false)
+      local line = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)[2]
+      eq('    username:', line)
+    end)
   end)
 end)
